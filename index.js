43e0cd6c2980a1a42ca50abc13ef7871e8829ea0
tarunranka <tarunranka.m@gmail.com>
2019-05-03 17:28:43 +0400

    1 file modified

--- index.js
+++ index.js
@@ -3,10 +3,6 @@ import { render } from 'react-dom';
 import './style.css';
 import useDropdown from './useDropdown';
 
-
-
-
-
 const App = () => {
   const Genders = ['Male', 'Female'];
   const [Gender, GenderComponents] = useDropdown('Gender', 'Male',Genders);
