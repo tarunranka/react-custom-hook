d9da27af368107b3a21b54021681d7be579e0d3f
tarunranka <tarunranka.m@gmail.com>
2019-05-03 17:28:13 +0400

    1 file modified

--- useDropdown.js
+++ useDropdown.js
@@ -5,6 +5,7 @@ const useDropdown = (label, defaultState, options) => {
   const id = `use-dropdown-${label.replace(" ", "").toLowerCase()}`;
   const Dropdown = () =>(
     <label htmlFor={id}>
+    {label}{' '}
       <select id={id} 
         value={state}
         onChange = {e => updateState(e.target.value)}
